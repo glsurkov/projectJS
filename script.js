86b27965f97f598c8d1e6592d1a7242a8c427f84
Ivan <example@gmail.com>
2021-09-11 23:05:33 +0500

    first commit #1

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 "use strict"
 
-console.log('arr' + ' - object');
-console.log(4 + '5');
+let numberOfFilms = prompt('Сколько фильмов посмотрели','');
 
-let incr = 10,
-    decr = 10;
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
 
-/*++incr;
---decr;*/
+let films = [];
+let evaluation = [];
+for (let i = 0; i < 2; i++)
+{
+    films[i] = prompt('Какой последний фильм посмотрели?','');
+    evaluation[i] = prompt('Какую оценку поставите?','');
+    personalMovieDB.movies[films[i]] = evaluation[i];
+}
 
-console.log(incr++);
-console.log(decr--);
+console.log(personalMovieDB.movies);
 
-console.log(2*4 === 8);
-
-const isChecked = true,
-    isClose = true;
 
