fb745b9393afba078e4c47f05486eee3feea8849
Ivan <example@gmail.com>
2021-09-12 01:11:19 +0500

    Second hometask

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 "use strict"
 
-let numberOfFilms = prompt('Сколько фильмов посмотрели','');
+
+let numberOfFilms = prompt('Сколько фильмов посмотрели', '');
 
 let personalMovieDB = {
-    count : numberOfFilms,
+    count : +numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
@@ -14,9 +15,33 @@ let films = [];
 let evaluation = [];
 for (let i = 0; i < 2; i++)
 {
-    films[i] = prompt('Какой последний фильм посмотрели?','');
-    evaluation[i] = prompt('Какую оценку поставите?','');
+    films[i] = prompt('Какой последний фильм посмотрели?', '');
+    while(films[i] == null || films[i].length > 50 || films[i] == '')
+    {
+        films[i] = prompt('Какой последний фильм посмотрели?', '');
+    }
+    do {
+        evaluation[i] = prompt('Какую оценку поставите?', '');
+       }
+    while(evaluation[i] == null || evaluation[i].length > 50 || evaluation[i] == '')
     personalMovieDB.movies[films[i]] = evaluation[i];
+}
+
+if (personalMovieDB.count < 10)
+{
+    console.log('Просмотрено довольно мало фильмов');
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30)
+{
+    console.log('Вы классчиеский зритель');
+}
+else if (personalMovieDB.count > 30)
+{
+    console.log('Вы киноман');
+}
+else
+{
+    console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB.movies);
